f60650b1499dd32dda13a4b2f5a7e06b548923a4
Daan Bouwman <daanbouwman19@gmail.com>
2026-04-11 21:54:32 +0200

    test(e2e): update webserver command to new web:dev script

--- playwright.config.ts
+++ playwright.config.ts
@@ -23,7 +23,7 @@ export default defineConfig({
     },
   ],
   webServer: {
-    command: 'npm run dev:all',
+    command: 'npm run web:dev',
     url: 'https://localhost:5173',
     reuseExistingServer: !process.env.CI,
     stdout: 'pipe',
